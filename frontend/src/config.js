8373112677986d39951a4c3e0f029ddfb2355c14
ayushpandey2711 <btech10081.19@bitmesra.ac.in>
2022-06-27 16:06:03 +0530

    my first commit

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,5 +1,5 @@
 import axios from "axios"
 
 export const axiosInstance= axios.create({
-    baseURL:"https://proshop-ayush.herokuapp.com/backend/",
+    baseURL:"https://proshop-ayush.herokuapp.com/api/",
 });
